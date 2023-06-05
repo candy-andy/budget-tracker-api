7c40a5254d3167c11d9252d3426456d1f37cab9f
Andrii Laptiiev <andy.megadev@gmail.com>
2023-06-05 10:09:23 +0300

    Initial commit

---
+++ src/main/java/com/candyandy/budgettrackerapi/BudgetTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.candyandy.budgettrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerApiApplication.class, args);
+	}
+
+}
