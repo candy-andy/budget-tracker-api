feec413d625bd0efd176437a76db3fd4d66a623e
Andrii Laptiiev <andy.megadev@gmail.com>
2023-07-03 23:59:27 +0300

    Add exception handler for user module and improve registration response

--- src/main/java/com/candyandy/budgettrackerapi/user/controller/RegistrationController.java
+++ src/main/java/com/candyandy/budgettrackerapi/user/controller/RegistrationController.java
@@ -5,11 +5,14 @@ import com.candyandy.budgettrackerapi.user.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,9 +22,11 @@ public class RegistrationController {
     private UserService userService;
 
     @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserRegistrationDto register(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
+    public ResponseEntity<Map<String, String>> register(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
         userService.save(userRegistrationDto);
-        return userRegistrationDto;
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Collections.singletonMap("email", userRegistrationDto.email()));
     }
 }
---
+++ src/main/java/com/candyandy/budgettrackerapi/user/exception/UserExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.candyandy.budgettrackerapi.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Collections;
+import java.util.Map;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    ResponseEntity<Map<String, String>> handleException(SQLIntegrityConstraintViolationException e) {
+        // TODO: add logging
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Collections.singletonMap("message", e.getMessage()));
+    }
+}
